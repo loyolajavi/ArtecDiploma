919a17ef3935b7d831e42fac76c6f12437b112fd
loyolajavi <loyolajavi@gmail.com>
2017-03-13 21:39:42 -0300

    #Agregado un ribbon Form

--- DiplomaSolucion/ARTEC.ENTIDADES/Solicitud.cs
+++ DiplomaSolucion/ARTEC.ENTIDADES/Solicitud.cs
@@ -9,29 +9,29 @@ namespace ARTEC.ENTIDADES
     public class Solicitud
     {
 
-        public int IdSolicitud { get; set; }
-        /// <summary>
-        /// Gets or sets the fecha inicio.
-        /// </summary>
-        /// <value>
-        /// The fecha inicio.
-        /// </value>
-        public DateTime FechaInicio { get; set; }
-        /// <summary>
-        /// Gets or sets the fecha fin.
-        /// </summary>
-        /// <value>
-        /// The fecha fin.
-        /// </value>
-        public DateTime FechaFin { get; set; }
-        public string Contacto { get; set; }
-        public Dependencia unaDependencia { get; set; }
-        public Prioridad unaPrioridad { get; set; }
-        public UsuarioSistema unResponsable { get; set; }
-        public Estado unEstado { get; set; }
-        public int MyProperty { get; set; }
+        //public int IdSolicitud { get; set; }
+        ///// <summary>
+        ///// Gets or sets the fecha inicio.
+        ///// </summary>
+        ///// <value>
+        ///// The fecha inicio.
+        ///// </value>
+        //public DateTime FechaInicio { get; set; }
+        ///// <summary>
+        ///// Gets or sets the fecha fin.
+        ///// </summary>
+        ///// <value>
+        ///// The fecha fin.
+        ///// </value>
+        //public DateTime FechaFin { get; set; }
+        //public string Contacto { get; set; }
+        //public Dependencia unaDependencia { get; set; }
+        //public Prioridad unaPrioridad { get; set; }
+        //public UsuarioSistema unResponsable { get; set; }
+        //public Estado unEstado { get; set; }
+        //public int MyProperty { get; set; }
 
-        //
+        ////
 
     }
 }
