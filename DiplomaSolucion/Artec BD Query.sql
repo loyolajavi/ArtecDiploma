4ac90e7d13e33cec001860862f713191621e839b
loyolajavi <loyolajavi@gmail.com>
2017-03-22 20:33:15 -0300

    #Creadas las tablas base (falta relacionarlas)

--- DiplomaSolucion/Artec BD Query.sql
+++ DiplomaSolucion/Artec BD Query.sql
@@ -22,6 +22,11 @@ create table Nota
 	Constraint [PK_Nota] primary key (IdNota)
 )
 
+
+alter table Nota
+add IdSolicitud int not null
+
+
 create table Cargo
 (
 	IdCargo int not null identity(1,1),
@@ -128,4 +133,222 @@ create table Inventario
 	Constraint [PK_Inventario] primary key (IdInventario)
 )
 
+
+
+create table Asignacion
+(
+	IdAsignacion int not null identity(1,1),
+	Fecha datetime not null,
+	Constraint [PK_Asignacion] primary key (IdAsignacion)
+)
+
+alter table Asignacion
+add IdDependencia int not null
+
+
+create table AsigDetalle
+(
+	IdAsigDetalle int not null identity(1,1),
+	IdAsignacion int not null,
+	IdInventario int not null,
+	IdSolicDetalle int null,
+	IdTipoAsignacion int not null,
+	IdAgente int null,
+	Observacion varchar(300) null,
+	TiempoAsignacion int null,
+	Constraint [PK_AsigDetalle] primary key (IdAsigDetalle, IdAsignacion)
+)
+
+
+
+create table TipoAsignacion
+(
+	IdTipoAsignacion int not null identity(1,1),
+	DescripTipoAsig varchar(100) not null,
+	Constraint [PK_TipoAsignacion] primary key (IdTipoAsignacion)
+)
+
+
+create table Marca
+(
+	IdMarca int not null identity(1,1),
+	DescripMarca varchar(200) not null,
+	Constraint [PK_Marca] primary key (IdMarca)
+)
+
+
+create table ModeloVersion
+(
+	IdModeloVersion int not null identity(1,1),
+	DescripModeloVersion varchar(300) not null,
+	Constraint [PK_ModeloVersion] primary key (IdModeloVersion)
+)
+
+
+
+create table EstadoInventario
+(
+	IdEstadoInventario int not null identity(1,1),
+	DescripEstadoInv varchar(50) not null,
+	Constraint [PK_EstadoInventario] primary key (IdEstadoInventario)
+)
+
+
+create table Deposito
+(
+	IdDeposito int not null identity(1,1),
+	NombreDeposito varchar(100) not null,
+	Direccion varchar(500) not null,
+	Constraint [PK_Deposito] primary key (IdDeposito)
+)
+
+
+create table Adquisicion
+(
+	IdAdquisicion int not null identity(1,1),
+	FechaAdq datetime not null,
+	FechaCompra datetime not null,
+	NroFactura varchar(50) null,
+	NroExpediente varchar(50) null,
+	NroOrdenCompra varchar(50) null,
+	RutaDocumentos nvarchar(500) null,
+	MontoCompra money null,
+	IdTipoAdquisicion int not null,
+	IdRendicion int null,
+	IdProveedor int not null,
+	Constraint [PK_Adquisicion] primary key (IdAdquisicion)
+)
+
+
+create table TipoAdquisicion
+(
+	IdTipoAdquisicion int not null identity(1,1),
+	DescripTipoAdq varchar(100) not null,
+	Constraint [PK_TipoAdquisicion] primary key (IdTipoAdquisicion)
+)
+
+
+create table Proveedor
+(
+	IdProveedor int not null identity(1,1),
+	NombreProveedor varchar(100) not null,
+	ContactoProveedor varchar(100) null,
+	MailProveedor nvarchar(200) null,
+	Constraint [PK_Proveedor] primary key (IdProveedor)
+)
+
+
+create table Telefono
+(
+	IdTelefono int not null identity(1,1),
+	CodArea int not null,
+	NroTelefono int not null,
+	IdTipoTelefono int not null,
+	Constraint [PK_Telefono] primary key (IdTelefono)
+)
+
+
+create table TipoTelefono
+(
+	IdTipoTelefono int not null identity(1,1),
+	DescripTipoTel varchar(50) not null,
+	Constraint [PK_TipoTelefono] primary key (IdTipoTelefono)
+)
+
+
+create table Direccion
+(
+	IdDireccion int not null identity(1,1),
+	Calle varchar(100) not null,
+	NumeroCalle varchar(100) not null,
+	Localidad varchar(100) null,
+	IdProvincia int null,
+	Constraint [PK_Direccion] primary key (IdDireccion)
+)
+
+
+create table Provincia
+(
+	IdProvincia int not null identity(1,1),
+	NombreProvincia varchar(100) not null,
+	Constraint [PK_Provincia] primary key (IdProvincia)
+)
+
+create table Cotizacion
+(
+	IdCotizacion int not null identity(1,1),
+	MontoCotizado money not null,
+	FechaCotizacion datetime not null,
+	IdProveedor int not null,
+	Constraint [PK_Cotizacion] primary key (IdCotizacion)
+)
+
+
+create table Partida
+(
+	IdPartida int not null identity(1,1),
+	MontoSolicitado money not null,
+	MontoOtorgado money null,
+	NroPartida varchar(10) null,
+	FechaEnvio datetime not null,
+	FechaAcreditacion datetime null,
+	Constraint [PK_Partida] primary key (IdPartida)
+)
+
+
+create table PartidaDetalle
+(
+	IdPartidaDetalle int not null identity(1,1),
+	IdPartida int not null,
+	IdSolicDetalle int not null,
+	IdAdquisicion int null,
+	Constraint [PK_PartidaDetalle] primary key (IdPartidaDetalle, IdPartida)
+)
+
+
+create table Rendicion
+(
+	IdRendicion int not null identity(1,1),
+	MontoGasto money not null,
+	IdPartida int not null,
+	Constraint [PK_Rendicion] primary key (IdRendicion)
+)
+
+
+create table SolicDetalle
+(
+	IdSolicitudDetalle int not null identity(1,1),
+	IdSolicitud int not null,
+	IdSubCategoria int not null,
+	Cantidad int not null,
+	IdEstadoSolDetalle int not null,
+	Constraint [PK_SolicDetalle] primary key (IdSolicitudDetalle, IdSolicitud)
+)
+
+
+create table RelCotSolDetalle
+(
+	IdSolicitudDetalle int not null,
+	IdSolicitud int not null,
+	IdCotizacion int not null,
+	Constraint [PK_RelCotSolDetalle] primary key (IdSolicitudDetalle, IdSolicitud, IdCotizacion)
+)
+
+
+create table RelDepUsuarioCargo
+(
+	IdAgente int not null,
+	IdDependencia int not null,
+	IdCargo int not null,
+	Constraint [PK_RelDepenUsuario] primary key (IdAgente, IdDependencia, IdCargo)
+)
+
+
+create table RelSolDetalleUsuario
+(
+	IdSolicitudDetalle int not null,
+	IdSolicitud int not null,
+	IdAgente int not null,
+	Constraint [PK_RelSolDetalleUsuario] primary key (IdSolicitudDetalle, IdSolicitud, IdAgente)
+)
 
