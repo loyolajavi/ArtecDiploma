8c6746fa19aa55b8922affd85b33ec665b56d9b8
loyolajavi <loyolajavi@gmail.com>
2017-05-17 18:56:33 -0300

    #Agregué entidades Partida y PartidaDetalle.cs

---
+++ DiplomaSolucion/ARTEC.ENTIDADES/Partida.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARTEC.ENTIDADES
+{
+    public class Partida
+    {
+
+        public int IdPartida { get; set; }
+        public decimal MontoSolicitado { get; set; }
+        public decimal MontoOtorgado { get; set; }
+        public string NroPartida { get; set; }
+        public DateTime FechaEnvio { get; set; }
+        public DateTime FechaAcreditacion { get; set; }
+        private List<PartidaDetalle> _unasPartidasDetalles = new List<PartidaDetalle>();
+
+        public List<PartidaDetalle> unasPartidasDetalles
+        {
+            get { return _unasPartidasDetalles; }
+            set { _unasPartidasDetalles = value; }
+        }
+
+        
+
+        
+
+    }
+}
---
+++ DiplomaSolucion/ARTEC.ENTIDADES/PartidaDetalle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARTEC.ENTIDADES
+{
+    public class PartidaDetalle
+    {
+
+        public int IdPartidaDetalle { get; set; }
+        public int IdPartida { get; set; }
+        public SolicDetalle SolicDetalleAsociado { get; set; }
+        
+        private List<Cotizacion> _unasCotizaciones = new List<Cotizacion>();
+
+        public List<Cotizacion> unasCotizaciones
+        {
+            get { return _unasCotizaciones; }
+            set { _unasCotizaciones = value; }
+        }
+
+        
+
+    }
+}
