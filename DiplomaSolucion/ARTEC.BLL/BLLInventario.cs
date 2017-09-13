dc0901bc6ca1c87b0bc1bb138b0f26f2bae7389e
loyolajavi <loyolajavi@gmail.com>
2017-09-13 20:10:10 -0300

    -Archivo q faltaba commitear recien

---
+++ DiplomaSolucion/ARTEC.BLL/BLLInventario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ARTEC.ENTIDADES;
+using ARTEC.DAL;
+
+namespace ARTEC.BLL
+{
+    public class BLLInventario
+    {
+
+        DALInventario GestorInventario = new DALInventario();
+
+        public List<Inventario> InventariosTraerListosParaAsignar(int IdSolicitud)
+        {
+            return GestorInventario.InventariosTraerListosParaAsignar(IdSolicitud);
+        }
+
+
+    }
+}
